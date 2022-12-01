b4ffe3583bbb8f3d26cd12946aec4c501cd02ece
HoLLy <dario.db@hotmail.com>
2022-12-01 13:25:21 +0100

    Make code more modular for future days

---
+++ src/day01/mod.rs
@@ -0,0 +1,90 @@
+use super::*;
+
+pub struct Day;
+
+impl AocDay<u32> for Day {
+    const DAY: u32 = 1;
+
+    const INPUT_SAMPLE: &'static str = include_str!("input_sample.txt");
+
+    const INPUT_REAL: &'static str = include_str!("input_real.txt");
+
+    fn calculate_silver(input: &str) -> u32 {
+        let mut lines = input.trim().lines().peekable();
+        let mut max = 0;
+
+        while lines.peek().is_some() {
+            let sum = lines
+                .by_ref()
+                .take_while(|&l| !l.is_empty())
+                .flat_map(str::parse::<u32>)
+                .sum();
+
+            max = max.max(sum);
+        }
+
+        max
+    }
+}
+
+impl AocDayFull<u32> for Day {
+    fn calculate_gold(input: &str) -> u32 {
+        let mut lines = input.trim().lines().peekable();
+
+        // note: order is from least to most
+        let mut max = [0, 0, 0];
+
+        while lines.peek().is_some() {
+            let sum = lines
+                .by_ref()
+                .take_while(|&l| !l.is_empty())
+                .flat_map(str::parse::<u32>)
+                .sum();
+
+            if sum <= max[0] {
+                continue;
+            }
+
+            if sum <= max[1] {
+                max[0] = sum;
+                continue;
+            }
+
+            if sum <= max[2] {
+                max[0] = max[1];
+                max[1] = sum;
+                continue;
+            }
+
+            max[0] = max[1];
+            max[1] = max[2];
+            max[2] = sum;
+        }
+
+        max.into_iter().sum()
+    }
+}
+
+#[test]
+fn test_day_1_silver_sample() {
+    let output = Day::calculate_silver(Day::INPUT_SAMPLE);
+    assert_eq!(24000, output);
+}
+
+#[test]
+fn test_day_1_silver_real() {
+    let output = Day::calculate_silver(Day::INPUT_REAL);
+    assert_eq!(67633, output);
+}
+
+#[test]
+fn test_day_1_gold_sample() {
+    let output = Day::calculate_gold(Day::INPUT_SAMPLE);
+    assert_eq!(45000, output);
+}
+
+#[test]
+fn test_day_1_gold_real() {
+    let output = Day::calculate_gold(Day::INPUT_REAL);
+    assert_eq!(199628, output);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,64 @@
+#![allow(clippy::wildcard_imports)]
+
+use std::fmt::Display;
+
+#[cfg(feature = "day01")]
+mod day01;
+
 fn main() {
-    let input = include_str!("input.txt");
-    let output = do_day_1_silver(input);
-    println!("Silver: {output}");
-    let output = do_day_1_gold(input);
-    println!("gold: {output}");
+    #[cfg(feature = "day01")]
+    day01::Day::execute_gold();
 }
 
-fn do_day_1_silver(data: &str) -> u32 {
-    let mut lines = data.trim().lines().peekable();
-    let mut max = 0;
+trait AocDay<T: Display> {
+    const DAY: u32;
+    const INPUT_SAMPLE: &'static str;
+    const INPUT_REAL: &'static str;
 
-    while lines.peek().is_some() {
-        let sum = lines
-            .by_ref()
-            .take_while(|&l| !l.is_empty())
-            .flat_map(|l| l.parse::<u32>())
-            .sum();
+    fn execute_silver() {
+        println!("Executing silver-only");
 
-        max = max.max(sum);
+        println!(
+            "Day {}, silver (sample): {}",
+            Self::DAY,
+            Self::calculate_silver(Self::INPUT_SAMPLE)
+        );
+        println!(
+            "Day {}, silver: {}",
+            Self::DAY,
+            Self::calculate_silver(Self::INPUT_REAL)
+        );
     }
 
-    max
+    fn calculate_silver(input: &str) -> T;
 }
 
-fn do_day_1_gold(data: &str) -> u32 {
-    let mut lines = data.trim().lines().peekable();
+trait AocDayFull<T: Display>: AocDay<T> {
+    fn execute_gold() {
+        println!("Executing full");
 
-    // note: order is from least to most
-    let mut max = [0, 0, 0];
+        println!(
+            "Day {}, silver (sample): {}",
+            Self::DAY,
+            Self::calculate_silver(Self::INPUT_SAMPLE)
+        );
+        println!(
+            "Day {}, silver: {}",
+            Self::DAY,
+            Self::calculate_silver(Self::INPUT_REAL)
+        );
 
-    while lines.peek().is_some() {
-        let sum = lines
-            .by_ref()
-            .take_while(|&l| !l.is_empty())
-            .flat_map(|l| l.parse::<u32>())
-            .sum();
-
-        if sum <= max[0] {
-            continue;
-        }
-
-        if sum <= max[1] {
-            max[0] = sum;
-            continue;
-        }
-
-        if sum <= max[2] {
-            max[0] = max[1];
-            max[1] = sum;
-            continue;
-        }
-
-        max[0] = max[1];
-        max[1] = max[2];
-        max[2] = sum;
+        println!(
+            "Day {}, gold (sample): {}",
+            Self::DAY,
+            Self::calculate_gold(Self::INPUT_SAMPLE)
+        );
+        println!(
+            "Day {}, gold: {}",
+            Self::DAY,
+            Self::calculate_gold(Self::INPUT_REAL)
+        );
     }
 
-    max.into_iter().sum()
-}
-
-#[test]
-fn test_day_1_silver_sample() {
-    let input = include_str!("sample.txt");
-    let output = do_day_1_silver(input);
-    assert_eq!(24000, output);
-}
-
-#[test]
-fn test_day_1_silver_real() {
-    let input = include_str!("input.txt");
-    let output = do_day_1_silver(input);
-    assert_eq!(67633, output);
-}
-
-#[test]
-fn test_day_1_gold_sample() {
-    let input = include_str!("sample.txt");
-    let output = do_day_1_gold(input);
-    assert_eq!(45000, output);
-}
-
-#[test]
-fn test_day_1_gold_real() {
-    let input = include_str!("input.txt");
-    let output = do_day_1_gold(input);
-    assert_eq!(199628, output);
+    fn calculate_gold(input: &str) -> T;
 }
